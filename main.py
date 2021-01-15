f9914b53bff053cdffbb1388a1d1288f05f8c301
Kristen Bertrand <bertrk@rpi.edu>
2021-01-15 17:42:57 -0500

    Update main.py
    rough structure of program, added setup function

---
+++ main.py
@@ -0,0 +1,38 @@
+from Question import Question
+import pyplot as plt
+
+'''
+Welcome to the Boyfriend Interrogator! 
+'''
+def setup():
+    print("Welcome! First, a few questions for the administrator:")
+    adminName = input("What is your name?").strip().centertitle()
+    print("Welcome, {}!".format(adminName))
+
+    qtypes = ['personal', 'easy', 'medium', 'hard']
+    questionObjects = []
+    for level in qtypes:
+        temp = input("File for {} questions? (hit enter if none)".format(level)).strip()
+        if temp:
+            questionObjects.append(Question(level, temp))
+
+    return adminName, questionObjects
+
+if __name__==__main__:
+    adminName, questions = setup()
+    
+    '''
+    Personal Questions (name, age, etc)
+    '''
+
+    '''
+    Easy Questions
+    '''
+
+    '''
+    Medium Questions
+    '''
+
+    '''
+    Hard Questions
+    '''
